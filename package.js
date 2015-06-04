0ab971865af83142f27a123516e8895aa2f180a7
Erik Ušaj <erikusaj@hotmail.com>
2015-06-04 20:43:51 +0200

    README update

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: "ConnectSDK Cordova plugin package for Meteor.",
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/erikusaj/connectsdk.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
