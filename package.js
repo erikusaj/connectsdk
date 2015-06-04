57d9acf7b7166b16e51b221fb9fef1f4c742b27f
Erik Ušaj <erikusaj@hotmail.com>
2015-06-04 20:25:08 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,30 @@
+
+Package.describe({
+  name: 'erikusaj:connectsdk',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: "ConnectSDK Cordova plugin package for Meteor.",
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Cordova.depends({
+  'com.connectsdk.cordovaplugin': 'https://github.com/ConnectSDK/Connect-SDK-Cordova-Plugin/tarball/f85f65378531f25db5438629e05482f4a8b136df'
+});
+
+Package.onUse(function(api) {
+  api.export('ConnectSDK');
+  api.versionsFrom('1.1.0.2');
+  api.use(["templating", "session", "ui", "blaze", "less", "reactive-var"]);
+//  api.versionsFrom("METEOR@0.9.2");
+  api.addFiles('connectsdk.js');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('erikusaj:connectsdk');
+  api.addFiles('connectsdk-tests.js');
+});
