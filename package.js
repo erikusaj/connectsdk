89f0d883ddb299c801bbb5fb1180a1c413c4bc43
Erik Ušaj <erikusaj@hotmail.com>
2015-06-05 00:52:49 +0200

    guesswork

--- package.js
+++ package.js
@@ -16,8 +16,11 @@ Cordova.depends({
 });
 
 Package.onUse(function(api) {
-//  api.export('ConnectSDK', 'web.cordova');
-  api.export('Command', 'web.cordova');
+  api.export('ConnectSDK', 'web.cordova');
+
+  //pure guesswork as documentation is vague
+
+/*  api.export('Command', 'web.cordova');
   api.export('Subscription', 'web.cordova');
   api.export('LaunchSession', 'web.cordova');
   api.export('DevicePicker', 'web.cordova');
@@ -28,13 +31,13 @@ Package.onUse(function(api) {
   api.export('Services', 'web.cordova');
 
 
-  api.export('discoveryManager', 'web.cordova');
+  api.export('discoveryManager', 'web.cordova');*/
 
   api.versionsFrom('1.1.0.2');
-  api.use(["templating", "session", "ui", "blaze", "less", "reactive-var"]);
+  api.use(["cordovaplugin"]);
 
 //  api.versionsFrom("METEOR@0.9.2");
-  api.addFiles('connectsdk.js', 'web.cordova');
+  api.addFiles('www/connectsdk.js', 'web.cordova');
 });
 
 Package.onTest(function(api) {
