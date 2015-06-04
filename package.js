629cf657beec51bcb1abf91dd5df1375b72220b7
Erik Ušaj <erikusaj@hotmail.com>
2015-06-04 21:50:48 +0200

    updating

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   name: 'erikusaj:connectsdk',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: "ConnectSDK Cordova plugin package for Meteor.",
+  summary: "DO NOT USE!  -developmet- ConnectSDK Cordova plugin package for Meteor.",
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/erikusaj/connectsdk.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -16,9 +16,23 @@ Cordova.depends({
 });
 
 Package.onUse(function(api) {
-  api.export('ConnectSDK', 'web.cordova');
+//  api.export('ConnectSDK', 'web.cordova');
+  api.export('Command', 'web.cordova');
+  api.export('Subscription', 'web.cordova');
+  api.export('LaunchSession', 'web.cordova');
+  api.export('DevicePicker', 'web.cordova');
+  api.export('ConnectableDevice', 'web.cordova');
+  api.export('CapabilityFilter', 'web.cordova');
+  api.export('PairingLevel', 'web.cordova');
+  api.export('AirPlayServiceMode', 'web.cordova');
+  api.export('Services', 'web.cordova');
+
+
+  api.export('discoveryManager', 'web.cordova');
+
   api.versionsFrom('1.1.0.2');
   api.use(["templating", "session", "ui", "blaze", "less", "reactive-var"]);
+
 //  api.versionsFrom("METEOR@0.9.2");
   api.addFiles('connectsdk.js', 'web.cordova');
 });
